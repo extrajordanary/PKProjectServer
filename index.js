e519250e3b8a70d12ec5bbad7d8c00f7bdaa1072
Jordan Arnesen <jordan101112@gmail.com>
2014-10-02 14:41:53 -0700

    uncommenting out the mongodb stuff that doesn't seem to work

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.use(express.static(path.join(__dirname, 'public')))
 // set up MongoDB access
 var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb'
 mongo.Db.connect(mongoUri, function (err, db) {
-	// db.collection('mydocs', function (er, collection) {
-	// 	collection.insert({'mykey': 'myvalue'}, {safe: true}, function (er, rs) {
-	// 	});
-	// });
+	db.collection('mydocs', function (er, collection) {
+		collection.insert({'mykey': 'myvalue'}, {safe: true}, function (er, rs) {
+		});
+	});
 })
 
 app.use(express.static(__dirname + '/public'))
