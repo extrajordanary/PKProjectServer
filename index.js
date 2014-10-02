27092245c54dba989d69b37cf6fbda94c4b12301
Jordan Arnesen <jordan101112@gmail.com>
2014-10-02 14:51:36 -0700

    commented out all mongo stuff

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express')
 	, http = require('http')
-	, mongo = require('mongodb')
+	// , mongo = require('mongodb')
 	, path = require('path')
 
 var app = express()
@@ -8,13 +8,13 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(path.join(__dirname, 'public')))
 
 // set up MongoDB access
-var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb'
-mongo.Db.connect(mongoUri, function (err, db) {
-	db.collection('mydocs', function (er, collection) {
-		collection.insert({'mykey': 'myvalue'}, {safe: true}, function (er, rs) {
-		});
-	});
-})
+// var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb'
+// mongo.Db.connect(mongoUri, function (err, db) {
+	// db.collection('mydocs', function (er, collection) {
+	// 	collection.insert({'mykey': 'myvalue'}, {safe: true}, function (er, rs) {
+	// 	});
+	// });
+// })
 
 app.use(express.static(__dirname + '/public'))
 
