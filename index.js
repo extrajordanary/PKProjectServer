da94738e7036a780dd77772a0b9a9e14b6c9c96c
Jordan Arnesen <jordan101112@gmail.com>
2014-10-02 14:36:43 -0700

    refollowing tutorial to add functionality; adding node_module subfiles

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 var express = require('express')
-var app = express();
+	, http = require('http')
+	, mongo = require('mongodb')
+	, path = require('path')
 
+var app = express()
 app.set('port', (process.env.PORT || 5000))
+app.use(express.static(path.join(__dirname, 'public')))
+
+// set up MongoDB access
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb'
+mongo.Db.connect(mongoUri, function (err, db) {
+	// db.collection('mydocs', function (er, collection) {
+	// 	collection.insert({'mykey': 'myvalue'}, {safe: true}, function (er, rs) {
+	// 	});
+	// });
+})
+
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('<html><body><h1>Hello World</h1></body></html>');
-});
+  response.send('<html><body><h1>Hello World</h1></body></html>')
+})
+
+app.use(function (request, response) {
+	response.render('404', {url:request.url})
+})
 
 app.listen(app.get('port'), function() {
   console.log("PKProjectServer is running at localhost:" + app.get('port'))
